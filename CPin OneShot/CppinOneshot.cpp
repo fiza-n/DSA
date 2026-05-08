47d77cec6be3ff701b4e7755022beeb2a948281a
fiza noor <fnoor034726@gmail.com>
2026-05-08 13:18:53 +0500

    if-else completed

--- CPin OneShot/CppinOneshot.cpp
+++ CPin OneShot/CppinOneshot.cpp
@@ -38,10 +38,10 @@ void EvenOdd(int  arr[], int size){
 // }
 
 int main(){
-    int size = 5;
-    int arr[size];
+    // int size = 5;
+    // int arr[size];
     
-    EvenOdd(arr, size);
+    // EvenOdd(arr, size);
 
     // int size = 5;
     // int arr[size];
@@ -57,6 +57,53 @@ int main(){
     // }
     // Saymyname();
     // return 0;
-    int x = 10;
-    cout<< "value of x is:"  << x << endl;
+    // int x = 10;
+    // cout<< "value of x is:"  << x << endl;
+
+    // string str;
+    // getline(cin, str);
+    // cout<<str<<endl;
+
+    // int age;
+    // cout<<"Enter your age:\n";
+    // cin>> age;
+    // if(age >= 18) {
+    //     cout<<" you are adult.\n";
+    // }
+    // else{
+    //     cout<<"you are underage";
+    // }
+
+    int marks;
+    cout<< "Enter you marks:\n";
+    cin>> marks;
+    if(marks<0 && marks > 100){
+        cout<<"invalid marks\n";
+    }
+    
+    else if(marks>= 80 && marks <=100){
+        cout<<"you got grade A+\n";
+    }
+    else if(marks>=60 && marks<=79){
+        cout<<"you got grade B\n";
+    }
+    else if(marks>=50 && marks<=59){
+        cout<<"you got grade C\n";
+    }
+    else if(marks>=45 && marks<=49){
+        cout<<"you got grade D\n";
+    }
+    else if(marks>=25 && marks<=44){
+        cout<<"you got grade E\n";
+    }
+    else if(marks<25){
+        cout<<"you got grade F\n";
+    }
+    else{
+        cout<<"Invalid marks\n";
+    }
+    
+
+
 }
+
